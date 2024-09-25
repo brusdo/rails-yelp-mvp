2da9d78376325206864cc4e99bafe49636d0f14e
Bruna de Oliveira <bruna.soliveira@icloud.com>
2024-09-25 15:09:25 +0100

    Rail Yelp MVP in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,32 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/new'
+  get 'reviews/show'
+  get 'reviews/show'
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  # read (all)
+  get 'restaurants', to: 'restaurants#index'
+
+  # create
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
+
+  # read (one)
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  Rails.application.routes.draw do
+    resources :restaurants do
+      resources :reviews, only: [:index, :new]
+    end
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+restaurants = [
+  {name: "Chocolate", address: "7 Boundary St, London E2 7JE", category: "belgian"},
+  {name: "Rishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"},
+  {name: "The Bear", address: "7 Boundary St, London E2 7JE", category: "japanese"},
+  {name: "Jimmy's Pizza", address: "7 Boundary St, London E2 7JE", category: "italian"},
+  {name: "Slugs and Snails", address: "7 Boundary St, London E2 7JE", category: "french"},
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  review = Review.new(content: "yummy yummy", rating: 4)
+  review.restaurant = restaurant
+  review.save
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
